e403c4bf4e113e343cebd0120461f3b96457fd4c
annemiekeSegersNovi <93577704+annemiekeSegersNovi@users.noreply.github.com>
2022-01-31 14:00:45 +0100

    opdrachten gemaakt

--- variables.js
+++ variables.js
@@ -7,38 +7,41 @@ const start = "Let's go!";
 // ==========================================
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
+const myName = "Annemieke";
 
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
+const eyeColor = "bruin";
 
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let myAge = 37;
 
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let myProffesion = "student";
 
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
+const myShoeSize = 38;
 
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let hasPartner = false;
 
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
+let myChildren = 0;
 
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
+console.log("Hallo, mijn naam is " + myName + " ik ben " + myAge + " jaar oud en ben " + myProffesion + "." );
+console.log("Ik ben een " + myProffesion + ". ik heb " + eyeColor + " ogen en schoenmaat " + myShoeSize + ".");
+console.log("Heeft partner: " + hasPartner + ". Heeft kinderen: " + myChildren);
